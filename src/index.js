93faed74664b3211cedd2d41bcc2cb51a3830139
Lauren Hoock <43796363+Kineara@users.noreply.github.com>
2022-04-30 01:16:17 -0600

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const taskForm = document.querySelector("#create-task-form");
+  const taskList = document.querySelector("#tasks");
+
+  taskForm.addEventListener("submit", addNewTask);
+
+  function addNewTask(e) {
+    e.preventDefault();
+
+    newListItem = document.createElement("li");
+    newListItem.textContent = taskForm.querySelector('#new-task-description').value;
+    taskList.appendChild(newListItem);
+    taskForm.reset();
+  }
 });
