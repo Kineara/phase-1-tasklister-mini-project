7b532a7e4875d2e9d1cdfcb3c10fed486cc4a94a
Lauren Hoock <43796363+Kineara@users.noreply.github.com>
2022-04-30 02:12:22 -0600

    Code cleanup

--- src/index.js
+++ src/index.js
@@ -3,16 +3,22 @@ document.addEventListener("DOMContentLoaded", () => {
   const taskForm = document.querySelector("#create-task-form");
   const taskList = document.querySelector("#tasks");
 
-  taskForm.addEventListener("submit", addNewTask);
+  taskForm.addEventListener("submit", submitHandler);
 
-  function addNewTask(e) {
+  function submitHandler(e) {
     e.preventDefault();
 
-    const newListItem = document.createElement("li");
     const newListItemText = taskForm.querySelector('#new-task-description').value;
-    newListItem.textContent = `${newListItemText} `
-    newListItem.appendChild(addDeleteBtn());
 
+    if (newListItemText) {
+      addTaskItem(newListItemText);
+    }
+  }
+
+  function addTaskItem(task) {
+    const newListItem = document.createElement("li");
+    newListItem.textContent = `${task} `;
+    newListItem.appendChild(addDeleteBtn());
     taskList.appendChild(newListItem);
     taskForm.reset();
   }
